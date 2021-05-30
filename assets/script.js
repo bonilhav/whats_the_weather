e846abdfea0eb1e0fa4edc18b7e38a925190606e
Vanessa Bonilha <81935432+bonilhav@users.noreply.github.com>
2021-05-30 12:17:07 -0600

    css updats for dynamic html, code for generating forcast, mods to index to accommodate new dynamic script

--- assets/script.js
+++ assets/script.js
@@ -1,64 +1,107 @@
 const current = $('.todaysWeather');
 const forecast = $('.futureWeather');
+const forecast1 = $('.futureWeather1');
+const forecast2 = $('.futureWeather2');
+const forecast3 = $('.futureWeather3');
+const forecast4 = $('.futureWeather4');
 
 
-$("#search").click(function(){
+$("#search").click(function () {
     event.preventDefault();
     let city = $('#search-city').val();
     getCurrentWeather(city);
-    /* getFutureWeather(city); */
+    /*  getFutureWeather(lat,lon); */
 });
 
 function getCurrentWeather(city) {
     let cityUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=7c86f0d48713fa92ff0fb20c045e6202`;
 
     $.get(cityUrl)
-    .then(function(data) {
-        console.log(data)
-        let today = moment().format("MM/DD/YYYY");
-        console.log(today)
+        .then(function (data) {
+            console.log(data)
+            let today = moment().format("MM/DD/YYYY");
+            console.log(today)
 
-        current.html(`
-        <h2>${data.name} ${today} ${data.weather[0].icon}</h2> 
+            let icon = data.weather[0].icon
+
+            current.html(`
+        <h2>${data.name} ${today} <img src="http://openweathermap.org/img/wn/${icon}@2x.png"/></h2> 
         <p>Temperature: ${data.main.temp}&#176;F</p>
         <p>Humidity: ${data.main.humidity}%</p>
         <p>Wind Speed: ${data.wind.speed} mph</p>
-        `/* , returnUVI(response.coord) */)
-        /* createHistory(response.name); */
-        let lat = data.coord.lat;
-        console.log(lat)
-        let lon = data.coord.lon;
-        console.log(lon)
-    });
+        `, returnUVI(data.coord.lat, data.coord.lon))
+        /* createHistory(response.name) */;
+            let lat = data.coord.lat;
+            console.log(lat)
+            let lon = data.coord.lon;
+            console.log(lon)
+            getFutureWeather(lat, lon);
+        });
 };
 
-function returnUVI () {
-    let uviUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=7c86f0d48713fa92ff0fb20c045e6202"
+function returnUVI(lat, lon) {
+    let uviUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=7c86f0d48713fa92ff0fb20c045e6202`
 
     $.get(uviUrl)
-    .then(function(data) {
-        console.log(data)
-    })
+        .then(function (data) {
+            console.log(data.daily)
+
+            current.append(
+                `<p>UVI: ${data.current.uvi} </p>`)
+            console.log(data.current.uvi)
+        })
 };
 
-/* function getFutureWeather(city) {
-    let futureCityUrl = `api.openweathermap.org/data/2.5/forecast?q=${city}&appid=6f9d945ac816d5bdf19278fb1c1a99bc`;
+function getFutureWeather(lat, lon) {
+    let futureCityUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=hourly,minutely,alerts&appid=7c86f0d48713fa92ff0fb20c045e6202`
 
     $.get(futureCityUrl)
-    .then(function(data) {
-        console.log(data)
-        let date = moment().format("MM/DD/YYYY");
-        console.log(date)
+        .then(function (data) {
+            console.log(data)
 
-        forecast.html(`
-        <h2>${data.name} ${date} ${data.weather[0].icon}</h2> 
-        <p>Temperature: ${data.main.temp}&#176;F</p>
-        <p>Humidity: ${data.main.humidity}%</p>
-        <p>Wind Speed: ${data.wind.speed} mph</p>
-        `)
+            for (let i = 1; i < data.daily.length; i++) {
+                const element = new Date(data.daily[i].dt).toLocaleDateString()
 
-    });
-}; */
+                if (data.daily !== 0) {
+
+            forecast.html(`
+                <h3>${moment(data.daily[1].dt, "X").format("MM/DD/YYYY")}</h3> 
+                <img class="futureIcon" src="http://openweathermap.org/img/wn/${data.daily[1].weather[0].icon}@2x.png"/>
+                <p>Temperature: ${data.daily[1].temp.day}&#176;F</p>
+                <p>Humidity: ${data.daily[1].humidity}%</p>
+                <p>Wind Speed: ${data.daily[1].wind_speed} mph</p>`)
+
+            forecast1.html(`
+                <h3>${moment(data.daily[2].dt, "X").format("MM/DD/YYYY")}</h3> 
+                <img class="futureIcon" src="http://openweathermap.org/img/wn/${data.daily[2].weather[0].icon}@2x.png"/>
+                <p>Temperature: ${data.daily[2].temp.day}&#176;F</p>
+                <p>Humidity: ${data.daily[2].humidity}%</p>
+                <p>Wind Speed: ${data.daily[2].wind_speed} mph</p>`)
+
+            forecast2.html(`
+                <h3>${moment(data.daily[3].dt, "X").format("MM/DD/YYYY")}</h3> 
+                <img class="futureIcon" src="http://openweathermap.org/img/wn/${data.daily[3].weather[0].icon}@2x.png"/>
+                <p>Temperature: ${data.daily[3].temp.day}&#176;F</p>
+                <p>Humidity: ${data.daily[3].humidity}%</p>
+                <p>Wind Speed: ${data.daily[3].wind_speed} mph</p>`)
+            
+            forecast3.html(`
+                <h3>${moment(data.daily[4].dt, "X").format("MM/DD/YYYY")}</h3> 
+                <img class="futureIcon" src="http://openweathermap.org/img/wn/${data.daily[4].weather[0].icon}@2x.png"/>
+                <p>Temperature: ${data.daily[4].temp.day}&#176;F</p>
+                <p>Humidity: ${data.daily[4].humidity}%</p>
+                <p>Wind Speed: ${data.daily[4].wind_speed} mph</p>`)
+
+            forecast4.html(`
+                <h3>${moment(data.daily[5].dt, "X").format("MM/DD/YYYY")}</h3> 
+                <img class="futureIcon" src="http://openweathermap.org/img/wn/${data.daily[5].weather[0].icon}@2x.png"/>
+                <p>Temperature: ${data.daily[5].temp.day}&#176;F</p>
+                <p>Humidity: ${data.daily[5].humidity}%</p>
+                <p>Wind Speed: ${data.daily[5].wind_speed} mph</p>`)
+            }
+            }
+        });
+};
 
 /* const citiesFromLocalStorage = JSON.parse(window.localStorage.getItem("cityHistory")) || [];
 
